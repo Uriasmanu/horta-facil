7731359e5b272973c9cb843c350d4623d7122ba4
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-09-19 23:03:53 -0300

    Criação da tela de login

--- src/assets/Pages/Login/Login.jsx
+++ src/assets/Pages/Login/Login.jsx
@@ -1,13 +1,18 @@
 import LoginComponent from '../../Components/LoginComponent/LoginComponent';
 import './_Login.scss'
+import './_LoginMobile.scss'
 
 import banner from '../../../image/horta.png'
 
 const Login = () => {
     return (
         <div className="container-login">
-            <LoginComponent />
-            <img src={banner} alt="" />
+
+            <div className='component'>
+                <h1>Horta Facil</h1>
+                <LoginComponent />
+            </div>
+            <img src={banner} alt="banner" className='banner' />
         </div>
     )
 }
