ba12890ff463e9fb5c9735395e8f96d32ab7a0e9
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-09-21 21:35:17 -0300

    botao de registrar

--- src/assets/Components/BotaoRegistrar/BotaoRegistrar.jsx
+++ src/assets/Components/BotaoRegistrar/BotaoRegistrar.jsx
@@ -5,11 +5,9 @@ const BotaoRegistrar = () => {
 
     return (
         <div className="container-BotaoRegistrar">
-            <button className="fancy" >
-                <span className="top-key"></span>
-                <span className="text">Voltar</span>
-                <span className="bottom-key-1"></span>
-                <span className="bottom-key-2"></span>
+
+            <button>
+                REGISTRAR
             </button>
         </div>
     )
